942094d409b0167d48bbe4b3b7462660f9699b3e
Merve <babutine@yahoo.com>
2023-11-21 19:59:49 +0000

    feat: Implement GET request for coffee lover endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,7 @@ export const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to the Drinks API!')
 })
+
+app.get('/coffeelover', (req, res) => {
+    res.send('I like coffee!')
+})
--- src/controllers/home_request.test.ts
+++ src/controllers/home_request.test.ts
@@ -13,3 +13,15 @@ describe ('Test Home API endpoint request', () => {
         expect(res.text).toEqual(expectedText)
     })
 })
+describe ('Test Coffee Lover API endpoint request', () => {
+    test('GET should return the correct coffe lover message', async() => {
+        const expectedStatusCode = 200
+        const expectedText = 'I like coffee!'
+//Act
+        const res = await request(app).get('/coffeelover')
+//Assert
+        expect(res.statusCode).toEqual(200)
+        expect(res.text).toEqual(expectedText)
+    })
+}
+)
