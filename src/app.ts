3fa5e6e1bef31231648c452506d55b09e246375a
Merve <babutine@yahoo.com>
2023-11-22 17:06:58 +0000

    refactor: Implement GET request for /coffee endpoint with MVCS design considerations

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import {router} from './routes/routes'
 
 export const app = express()
 app.use(express.json())
@@ -11,10 +12,4 @@ app.get('/coffeelover', (req, res) => {
     res.send('I like coffee!')
 })
 
-
-app.get('/coffee', (req, res) => {
-    res.json({
-        drinkType: 'Coffee',
-        name: 'Latte'
-    })
-})
+app.use('/coffee', router)
