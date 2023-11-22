9d9ba1d546ce4ae46af120fa4af887388ee8ae3d
Merve <babutine@yahoo.com>
2023-11-22 17:39:35 +0000

    test: add tests for default and specified coffee

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,19 +1,32 @@
 //Arrange
+import request from "supertest";
+import { app } from "../app";
 
-import request from 'supertest'
-import {app} from '../app'
-
-describe('Test Coffee API endpoint request', () => {
-  const expectedStatusCode = 200
-  const expectedResponse = {
-    drinkType: "Coffee",
-    name: "Latte",
-  };
-  //Act
-  test('Get coffee should return correct object', async () => {
-    const res = await request(app).get("/coffee").query("{coffeeName: latte")
+describe("Test Coffee API endpoint request", () => {
+  test("Get coffee should return default coffee by default", async () => {
+    const expectedStatusCode = 200;
+    const expectedResponse = {
+      drinkType: "Coffee",
+      name: "Latte",
+    };
+    //Act
+    const res = await request(app).get("/coffee");
     //Assert
-    expect(res.statusCode).toEqual(expectedStatusCode)
-    expect(res.body).toEqual(expectedResponse)
-  })
-})
+    expect(res.statusCode).toEqual(expectedStatusCode);
+    expect(res.body).toEqual(expectedResponse);
+  });
+  test("Get coffee should return sepcified coffee requested", async () => {
+    const expectedStatusCode = 200;
+    const expectedResponse = {
+      drinkType: "Coffee",
+      name: "Espresso",
+    };
+    //Act
+    const res = await request(app)
+      .get("/coffee")
+      .query({coffeeName: 'Espresso'});
+    //Assert
+    expect(res.statusCode).toEqual(expectedStatusCode);
+    expect(res.body).toEqual(expectedResponse);
+  });
+});
